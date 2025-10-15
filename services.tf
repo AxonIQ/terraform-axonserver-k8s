5534a5a852108e84ac85d66b9abf5089b87f436d
Francesco Latini <francesco.latini@axoniq.io>
2025-10-15 15:41:31 +0200

    Added the chance to create a neg in case of GKE

--- services.tf
+++ services.tf
@@ -1,6 +1,13 @@
 resource "kubernetes_service" "axonserver" {
   count = var.nodes_number
 
+  lifecycle {
+    precondition {
+      condition     = !var.gke_neg || length(var.gke_neg_zone) > 0
+      error_message = "When gke_neg is enabled, gke_neg_zone must contain at least one zone. Please provide a list of zones or set gke_neg to false."
+    }
+  }
+
   metadata {
     name      = "${var.cluster_name}-${count.index + 1}"
     namespace = var.create_namespace ? kubernetes_namespace.axonserver[0].id : data.kubernetes_namespace.axonserver[0].id
@@ -9,11 +16,18 @@ resource "kubernetes_service" "axonserver" {
       run     = "${var.cluster_name}-${count.index + 1}"
       cluster = var.cluster_name
     }
-    annotations = {
-      "prometheus.io/scrape" = "true"
-      "prometheus.io/port"   = "8081"
-      "prometheus.io/path"   = "/actuator/prometheus"
-    }
+
+    annotations = merge(
+      {
+        "prometheus.io/scrape" = "true"
+        "prometheus.io/port"   = "8081"
+        "prometheus.io/path"   = "/actuator/prometheus"
+      },
+      var.gke_neg && length(var.gke_neg_zone) > 0 ? {
+        "cloud.google.com/neg"        = "{\"exposed_ports\": {\"8024\":{\"name\": \"k8s-${var.cluster_name}-${count.index + 1}-8024\"}}}"
+        "cloud.google.com/neg-status" = "{\"network_endpoint_groups\":{\"8024\":\"k8s-${var.cluster_name}-${count.index + 1}-8024\"},\"zones\":${jsonencode(var.gke_neg_zone)}}"
+      } : {}
+    )
   }
   spec {
     selector = {
